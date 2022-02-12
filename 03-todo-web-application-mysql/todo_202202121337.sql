bfe6d7c42fdb7e05578f01c8cc392122f35ab0e0
Gourab Sarkar <gourabsarkar.x@gmail.com>
2022-02-12 14:48:46 +0530

    added dummy DML

---
+++ 03-todo-web-application-mysql/todo_202202121337.sql
@@ -0,0 +1,7 @@
+INSERT INTO todos.todo (id,description,is_done,target_date,`user`) VALUES
+	 (1,'Read Harry Potter book 1',1,'2022-02-08 18:30:00','gourab'),
+	 (2,'Learn docker',1,'2022-02-14 18:30:00','gourab'),
+	 (3,'Learn microservices',0,'2022-02-27 18:30:00','gourab'),
+	 (4,'Chill in life',0,'2022-04-17 18:30:00','gourab'),
+	 (5,'Practice competitive programming',0,'2022-05-16 18:30:00','gourab'),
+	 (6,'Deploy to docker using link',1,'2022-02-11 00:00:00','gourab');
