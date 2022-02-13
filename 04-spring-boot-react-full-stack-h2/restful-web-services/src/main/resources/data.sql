46d0c314c42c978045e95278ca704fed979182aa
Gourab Sarkar <gourabsarkar.x@gmail.com>
2022-02-13 22:19:25 +0530

    Updated dummy data.sql

--- 04-spring-boot-react-full-stack-h2/restful-web-services/src/main/resources/data.sql
+++ 04-spring-boot-react-full-stack-h2/restful-web-services/src/main/resources/data.sql
@@ -4,9 +4,9 @@
 INSERT INTO USER (ID, USERNAME, PASSWORD, ROLE) 
 VALUES (1, 'in28minutes', '$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e','ROLE_USER');
 
-/*in28minutes2/mypassword*/
+/*gourab/password*/
 INSERT INTO USER (ID, USERNAME, PASSWORD, ROLE) 
-VALUES (2, 'in28minutes2', '$2a$10$i9AckmxMkb4yKtLCdxeQheCm2pXWB3qZ2G189/Ph/DUci1DvLO.Rq','ROLE_USER');
+VALUES (2, 'gourab', '$2a$10$fTanNcoaUWY3UHgz5sAaX.L5rHWzvpNCppLz43qkvQekh/MwPVN3.','ROLE_USER');
 
 
 
