9b3c8b9d39afabc619ad1878dc049aba365aa567
Gourab Sarkar <gourabsarkar.x@gmail.com>
2022-02-21 04:23:31 +0530

    Added spring-cloud-starter-netflix-eureka-client dependency in pom.xml for both currency projects. Enabled client discovery for netflix eureka naming server in both projects. Configured @FeignClient for connecting to netflix eureka naming server. Updated application.properties for netflix eureka naming server. Updated docker-compose.yaml for the above changes and added netflix eureka naming server service.

--- 05-microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
+++ 05-microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
@@ -4,8 +4,16 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "${CURRENCY_EXCHANGE_URI:http://localhost:8000}")
-//@FeignClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service", url = "${CURRENCY_EXCHANGE_URI:http://localhost:8000}")
+/*
+ * When 'url' attribute is not specified in the @FeignClient annotation, it will automatically ask the
+ * eureka-naming-server service registry for all the instances of "currency-exchange-service". 
+ * The FeignClient includes "ribbon" as dependency, which is a client side load balancer. Ribbon will 
+ * then automatically load balance requests across all the "currency-exchange-service" instances which 
+ * were returned by the eureka-naming-server service registry.
+ */
+@FeignClient(name = "currency-exchange-service")
+
 //@FeignClient(name="netflix-zuul-api-gateway-server")
 public interface CurrencyExchangeServiceProxy {
 
